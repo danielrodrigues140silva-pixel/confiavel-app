5f46b436e3e57f6b8e60ead9f8e01433cf9578aa
danielrodrigues140silva-pixel <daniel.rodrigues140silva@gmail.com>
2026-02-01 20:34:55 -0300

    Delete data directory

--- data/public/public/public/css/public/js/public/js/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('search').addEventListener('input', (e)=>{
-  const query = e.target.value.toLowerCase();
-  const features = document.getElementById('features');
-  features.innerHTML = `<p>Resultados da pesquisa para: ${query}</p>`;
-});
